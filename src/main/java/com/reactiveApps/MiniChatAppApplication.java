b1e3731e600914cb2ad34dcc238f6ff86a8b19e5
DXTkastb <dxtkastb@gmail.com>
2022-07-27 21:14:00 +0530

    simple chat app created

---
+++ src/main/java/com/reactiveApps/MiniChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.reactiveApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactiveApps/MiniChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveApps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
